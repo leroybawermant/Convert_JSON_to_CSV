34429ddec2cd91ef0b31c07c2a55836c1e858d11
leroybawermant <155321232+leroybawermant@users.noreply.github.com>
2024-07-18 12:11:51 +0200

    Update converter.py

--- Convert_JSON_to_CSV/converter.py
+++ Convert_JSON_to_CSV/converter.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
         with open('output.csv', 'w') as f:
             f.write(output)
     except Exception as ex:
-        print(f'Error: {str(ex)}')
+        print(f'Error: {str(ex)}').......
